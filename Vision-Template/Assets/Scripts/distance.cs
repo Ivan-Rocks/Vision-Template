396270500588d552c3b0b423b728c34ec9fab9d4
张一凡 <yifan20@illinois.edu>
2020-12-18 19:20:17 +0800

    Version 0.1
    20201218Version

---
+++ Vision-Template/Assets/Scripts/distance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class distance : MonoBehaviour
+{
+    public GameObject stimulus;
+    public GameObject cell;
+    public double dist;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(stimulus.transform.position, cell.transform.position);
+        text.text = dist.ToString();
+        print(dist);
+    }
+}
